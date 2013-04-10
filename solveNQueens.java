6e3ba9d4263f1b20e169ea78ae514da21ea8f9a6
jingrui <jingrui192@gmail.com>
2013-04-10 00:07:51 -0500

    Update solveNQueens.java

--- solveNQueens.java
+++ solveNQueens.java
@@ -2,12 +2,13 @@ import java.util.ArrayList;
 
 
 public class Solution {
-  public static void main(String[] args){
+	public static void main(String[] args){
 		ArrayList<String[]> ret = solveNQueens(4);
-		for (int i = 0; i < ret.size(); i++)
+		for (int i = 0; i < ret.size(); i++){
 			for (int j = 0; j < ret.get(i).length; j++)
 				System.out.println(ret.get(i)[j]);
-
+		System.out.println();
+		}
 	}
     public static ArrayList<String[]> solveNQueens(int n) {
         ArrayList<String[]> ret = new ArrayList<String[]>();
@@ -15,8 +16,8 @@ public class Solution {
         helper(n,1,a,ret);
         return ret;
     }
-    public static boolean helper (int n, int cur,int [] ar,ArrayList<String[]> ret){
-        if ( cur > n){
+    public static void helper (int n, int cur,int [] ar,ArrayList<String[]> ret){
+        if (cur > n){
             String[] strbuf = new String[n];
             for (int i = 1; i <= n; i++){
                 String str = "";
@@ -28,17 +29,16 @@ public class Solution {
                 strbuf[i-1] = str;
             }
             ret.add(strbuf);
-            return true;
+            return;
         }
             
         for(int i = 1; i <= n; i++){
             if(check(cur,ar,i)){
                 ar[cur] = i;
-                if (helper(n,cur+1,ar,ret))
-                    return true;
+                helper(n,cur+1,ar,ret);
             }
         }
-        return false;
+        return;
     }
     public static boolean check(int cur,int[] ar,int j){
         for(int i = 1; i < cur; i++)
