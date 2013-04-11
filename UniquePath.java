5c8b4997395e7cf2479a1595aadd1e09cceaf465
jingrui <jingrui192@gmail.com>
2013-04-11 12:20:39 -0500

    Update UniquePath.java

--- UniquePath.java
+++ UniquePath.java
@@ -1,5 +1,18 @@
 // DP. O(n^2)
-
+public class Solution {
+    public int uniquePaths(int m, int n) {
+        int[][]ret = new int[m+1][n+1];
+        for(int i = 0; i <= m; i++)
+            ret[i][n] = 0;
+        for(int i = 0; i <= n; i++)
+            ret[m][i] = 0;
+        ret[m][n-1] = 1;
+        for(int i = n-1;i >= 0; i--)
+            for(int j = m-1; j >= 0; j--)
+                ret[j][i] = ret[j+1][i]+ret[j][i+1];
+        return ret[0][0];
+    }
+}
 // backtracking. Running time = O(2^n)
 public class Solution {
     public int uniquePaths(int m, int n) {
