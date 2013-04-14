86bc3c654a039e5e0e1b98b2b6fc9ba00b57ee2b
jingrui <jingrui192@gmail.com>
2013-04-14 15:33:02 -0500

    Update MinimumWindowSubstring.java

--- MinimumWindowSubstring.java
+++ MinimumWindowSubstring.java
@@ -1,15 +1,7 @@
 // O(n) running time
 // Work with Duplicate, OK for small input, but Time exceeded for large input
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.PriorityQueue;
-
-
 public class Solution {
-	public static void main(String[] args){
-		System.out.println(minWindow("dsohniaulobaxywggcojkvxubzslwxpcqfjfnjzyxcbwczfibttmosgqsadqysuqyevlpumpynnamsipndbewjwqmzimsdcsjzdyvrntebtsvdijglnexjujkfvcizwryarvthoykjsfoaowodbloxwzctdmuafsczpvgdvqooxcqmygrbtgbpffkksekzymynigsdwitukmilsbomh", "pdovozpul"));
-	}
-	public static String minWindow(String S, String T) {
+    public static String minWindow(String S, String T) {
         HashMap<Character,PriorityQueue<Integer>> hm 
             = new HashMap<Character,PriorityQueue<Integer>>();
         int t = T.length(), num = 0;
@@ -25,23 +17,32 @@ public class Solution {
         
         int dist = Integer.MAX_VALUE;
         int s = 0, e = 0, Amin, Amax;
+        PriorityQueue<Integer> all = new PriorityQueue<Integer>();
         for(int i = 0; i < S.length(); i++){
         	char cur = S.charAt(i);
             if(hm.containsKey(cur)){
             	PriorityQueue<Integer> h = hm.get(cur);
             	h.add(i);
+            	all.add(i);
                 hm.put(cur,h);
+                int polled = h.poll();
+                all.remove(polled);
                 // if new char
-            	if(h.poll() == -1)	num++;
-            	              
+            	if(polled == -1)	num++;
+            	
                 // if not have all char yet
                 if (num < t)	continue;
-                
+//                System.out.println("i = "+i+",num = "+num);
                 // if already have all
                 if (num == t){
                     // find the max and min of the value set
-                    Amin = findmin(hm);
-                    Amax = findmax(hm);
+                    Amin = all.peek();
+//                    Object[] a = all.toArray();
+//                    for(int j = 0; j < a.length;j++)
+//                        System.out.println("a["+j+"]="+a[j]);
+
+                    Amax = Collections.max(all);
+//                    System.out.println("max = "+Amax+", min = "+Amin);
                     if ((Amax-Amin+1) < dist){
                         dist = Amax-Amin+1;
                         s = Amin;
@@ -53,34 +54,7 @@ public class Solution {
         if (num < t)    return "";
         return S.substring(s,e+1);
     }
-	public static int findmin(HashMap<Character,PriorityQueue<Integer>> hm ){
-		int min = Integer.MAX_VALUE;
-		for(char c : hm.keySet()){
-			PriorityQueue<Integer> h = hm.get(c);
-        	min = Math.min(min, h.peek());
-		}
-		return min;
-	}
-	public static int findmax(HashMap<Character,PriorityQueue<Integer>> hm ){
-		int max = Integer.MIN_VALUE;
-		for(char c : hm.keySet()){
-        	max = Math.max(max, Collections.max(hm.get(c)));
-		}
-		return max;
-	}
-	/* 
-	// even O(1) to get the max and min, it is still Time Exceed.
-	public static int findmax(HashMap<Character,PriorityQueue<Integer>> hm ){
-		int max = Integer.MIN_VALUE;
-    		for(char c : hm.keySet()){
-			Object[] a = hm.get(c).toArray();
-        		max = Math.max(max, (Integer)a[a.length-1]);
-		}
-		return max;
-	}
-	*/
 }
-
 // this is good if T does not have duplicates
 public class Solution {
     public String minWindow(String S, String T) {
