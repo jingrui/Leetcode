21ddd0876722d31916006ec1d84ae4ea1a7f2d10
jingrui <jingrui192@gmail.com>
2013-04-14 20:07:50 -0500

    Create Combinations.java

---
+++ Combinations.java
@@ -0,0 +1,23 @@
+public class Solution {
+    public ArrayList<ArrayList<Integer>> combine(int n, int k) {
+        // Start typing your Java solution below
+        // DO NOT write main() function
+        if(n < k) return null;
+        ArrayList<ArrayList<Integer>> all = new  ArrayList<ArrayList<Integer>>();       
+        if(k == 1){
+            for(int i = 1;i <= n; i++){
+                ArrayList<Integer> al = new ArrayList<Integer>();
+                al.add(i);
+                all.add(al);
+            }
+            return all;
+        }
+        for(int i = n;i >= k; i--)          
+            for(ArrayList<Integer> al : combine(i-1,k-1)){
+                al.add(i);
+                all.add(al);
+            } 
+        
+        return all;       
+    }
+}
