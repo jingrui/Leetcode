f9d56240ea3d31c180be266684b851a2a0ee9101
jingrui <jingrui192@gmail.com>
2013-09-04 10:44:33 +0800

    Update Decode Ways.java

--- Decode Ways.java
+++ Decode Ways.java
@@ -2,10 +2,18 @@ public class Solution {
     public int numDecodings(String s) {
         // Start typing your Java solution below
         // DO NOT write main() function
-        if((s.length() == 1)||(s.length() == 0))  return 1;
-        if((s.charAt(0) -'0')*10+(s.charAt(1) -'0') <= 26 )
-            return numDecodings(s.substring(1))+numDecodings(s.substring(2));
-        else
-            return numDecodings(s.substring(1));
+        if(s.length() == 0)  return 0;
+        return helper(s);
+    }
+    public int helper(String s){
+        if(s.length() == 0)  return 1;
+
+        if (((s.charAt(0) -'0') >= 1) && ((s.charAt(0) -'0') <= 9)){
+            if(s.length() == 1)  return 1;
+            if((s.charAt(0) -'0')*10+(s.charAt(1) -'0') <= 26 )
+                return helper(s.substring(1))+helper(s.substring(2));
+            else
+                return helper(s.substring(1));
+        }else return 0;
     }
 }
